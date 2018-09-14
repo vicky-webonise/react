b02b00bfcc971915ccfd5350cdad4d9b4d869e98
Vicky Shinde <vicky.shinde@weboniselab.com>
2018-09-14 17:19:39 +0530

    Create header footer componants

--- react-kit/src/js/components/App.js
+++ react-kit/src/js/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Header from './common/Header';
+import Footer from './common/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h4>test</h4>
-      </div>
+      <React.Fragment>
+        <Header />
+        <div className="container">
+          <h4>test</h4>
+        </div>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
---
+++ react-kit/src/js/components/common/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="bg-light">
+        <div className="container">
+          footer
+        </div>
+      </div>
+    )
+  }
+}
