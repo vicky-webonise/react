9e995c34e67a3fbfe0ead8bf1bfd3f9b5d951a9d
Vicky Shinde <vicky.shinde@weboniselab.com>
2018-09-17 11:17:54 +0530

    resolved isRequired warning

--- react-kit/src/js/components/BlogDetail.js
+++ react-kit/src/js/components/BlogDetail.js
@@ -5,51 +5,53 @@ import PageLoader from './common/PageLoader';
 import API from '../constants/api-endpoint';
 
 export default class BlogDetail extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            postDetailData: [],
-            isLoading: true
-        };
-    }
+    this.state = {
+      postDetailData: {
+        title: 'title dummy'
+      },
+      isLoading: true
+    };
+  }
 
-    componentDidMount() {
-        this._getPostDetailData();
-    }
+  componentDidMount() {
+    this._getPostDetailData();
+  }
 
-    _getPostDetailData() {
-        let url = API.postId(this.props.match.params.postId)
-        axios.get(url)
-        .then(response => {
-            const postDetailData = response.data;
-            this.setState({
-                postDetailData,
-                isLoading: false
-            });
-            console.log(postDetailData)
-        })
+  _getPostDetailData() {
+    let url = API.postId(this.props.match.params.postId)
+    axios.get(url)
+      .then(response => {
+        const postDetailData = response.data;
+        this.setState({
+          postDetailData,
+          isLoading: false
+        });
+        console.log(postDetailData)
+      })
 
-        .catch(error => {
-            this.setState({
-                postDetailData: [],
-                isLoading: false
-            });
-            console.log(error);
-        })
-    }
+      .catch(error => {
+        this.setState({
+          postDetailData,
+          isLoading: false
+        });
+        console.log(error);
+      })
+  }
 
-    render() {
-        return (
-            <div>
-                {this.state.isLoading && <PageLoader />}
-                <InnerBanner title={this.state.postDetailData.title} />
-                <div className="container py-3">
-                    <h1>{this.state.postDetailData.title}</h1>
-                    <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
-                    <p>{this.state.postDetailData.body}</p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.isLoading && <PageLoader />}
+        <InnerBanner title={this.state.postDetailData.title} />
+        <div className="container py-3">
+          <h1>{this.state.postDetailData.title}</h1>
+          <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+          <p>{this.state.postDetailData.body}</p>
+        </div>
+      </div>
+    );
+  }
 }
--- react-kit/src/js/routes/route.js
+++ react-kit/src/js/routes/route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
 import Container from '../container/Container';
 import Home from '../components/Home';
@@ -7,17 +7,15 @@ import Blog from '../components/Blog';
 import BlogDetail from '../components/BlogDetail';
 
 export default class Root extends React.Component {
-    render() {
-        return (
-            <HashRouter>
-                <Switch>
-                <Container path='/'>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/blog" component={Blog} />
-                    <Route exact path="/blog/:postId" component={BlogDetail} />
-                </Container>
-                </Switch>
-            </HashRouter>
-        );
-    }
+  render() {
+    return (
+      <HashRouter>
+        <Container path='/'>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/blog" component={Blog} />
+          <Route exact path="/blog/:postId" component={BlogDetail} />
+        </Container>
+      </HashRouter>
+    );
+  }
 }
