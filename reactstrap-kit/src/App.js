2fc0018549b66cefb85f20926f7c284b388d836a
Vicky Shinde <vicky.shinde@weboniselab.com>
2018-10-04 19:06:01 +0530

    added sample chart.js

--- reactstrap-kit/src/App.js
+++ reactstrap-kit/src/App.js
@@ -1,43 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {Bar} from 'react-chartjs-2';
 import { Container, Row, Col, Button, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+          label: "My First dataset",
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      }
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-
-        <Button color="primary">primary</Button>{' '}
-        <Button color="secondary">secondary</Button>{' '}
-        <Button color="success">success</Button>{' '}
-        <Button color="info">info</Button>{' '}
-        <Button color="warning">warning</Button>{' '}
-        <Button color="danger">danger</Button>{' '}
-        <Button color="link">link</Button>
+        <Container>
+          <Row>
+            <Col xs='6'>
+              <Bar data={this.state.data} />
+            </Col>
+            <Col xs='6'>
+              <Button color="primary">primary</Button>{' '}
+              <Button color="secondary">secondary</Button>{' '}
+              <Button color="success">success</Button>{' '}
+              <Button color="info">info</Button>{' '}
+              <Button color="warning">warning</Button>{' '}
+              <Button color="danger">danger</Button>{' '}
+              <Button color="link">link</Button>
 
 
-        <Breadcrumb>
-          <BreadcrumbItem active>Home</BreadcrumbItem>
-        </Breadcrumb>
-        <Breadcrumb>
-          <BreadcrumbItem><a href="#">Home</a></BreadcrumbItem>
-          <BreadcrumbItem active>Library</BreadcrumbItem>
-        </Breadcrumb>
-        <Breadcrumb>
-          <BreadcrumbItem><a href="#">Home</a></BreadcrumbItem>
-          <BreadcrumbItem><a href="#">Library</a></BreadcrumbItem>
-          <BreadcrumbItem active>Data</BreadcrumbItem>
-        </Breadcrumb>
-
+              <Breadcrumb>
+                <BreadcrumbItem active>Home</BreadcrumbItem>
+              </Breadcrumb>
+              <Breadcrumb>
+                <BreadcrumbItem><a onClick={() => {}} href="javascript.void(0)">Home</a></BreadcrumbItem>
+                <BreadcrumbItem active>Library</BreadcrumbItem>
+              </Breadcrumb>
+              <Breadcrumb>
+                <BreadcrumbItem><a onClick={() => {}} href="javascript.void(0)">Home</a></BreadcrumbItem>
+                <BreadcrumbItem><a onClick={() => {}} href="javascript.void(0)">Library</a></BreadcrumbItem>
+                <BreadcrumbItem active>Data</BreadcrumbItem>
+              </Breadcrumb>
+              </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
